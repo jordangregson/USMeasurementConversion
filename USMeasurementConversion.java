41894a2b2a3e14aec5b0a50887a8771495b9bacb
Jordan Gregson <49405277+jordangregson@users.noreply.github.com>
2021-01-28 21:24:35 -0500

    Create USMeasurementConversion.java

---
+++ USMeasurementConversion.java
@@ -0,0 +1,10 @@
+public class USMeasurementConversion {
+    public static void main(String[] args) {
+                
+        double inches = 0;
+        double feet = 0;
+        double yards = 0;
+        double miles = 0;
+    }
+    
+}
